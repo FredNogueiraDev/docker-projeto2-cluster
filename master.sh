2d136345072158ea8d1e9187d4c2d1d517d9cb35
Frederico Nogueira <fredericonogueira01@outlook.com>
2022-10-13 10:50:54 -0300

    Projeto 2 Cluster com alterações feitas no script

--- master.sh
+++ master.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 sudo docker swarm init --advertise-addr=10.10.10.100
 sudo docker swarm join-token worker | grep docker > /vagrant/worker.sh
+docker volume create teste
+docker run -dti --mount type=volume,src=teste,dst=teste debian
+docker volume rm teste
